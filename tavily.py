5d4234581d9f9ed4027bc2dbfea24949e0c9277b
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-09-04 13:26:22 -0400

    initial commit

---
+++ tavily.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+class Client:
+    def __init__(self, api_key):
+        self.base_url = "http://tavilyapi.us-east-1.elasticbeanstalk.com/smart-search"
+        self.api_key = api_key
+        self.headers = {
+            "Content-Type": "application/json",
+            # any other headers you want
+        }
+
+    def _search(self, query, search_depth="basic", num_results=5,
+                include_domains=None, exclude_domains=None,
+                include_answer=False, include_raw_content=False):
+        """
+        Internal search method to send the request to the API.
+        """
+        data = {
+            "query": query,
+            "search_depth": search_depth,
+            "include_answer": include_answer,
+            "include_raw_content": include_raw_content,
+            "num_results": num_results,
+            "include_domains": include_domains or [],
+            "exclude_domains": exclude_domains or [],
+            "api_key": self.api_key
+        }
+        response = requests.post(self.base_url, data=json.dumps(data), headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()  # Raises a HTTPError if the HTTP request returned an unsuccessful status code
+
+    def basic_search(self, query, **kwargs):
+        """
+        Basic search method.
+        """
+        return self._search(query, search_depth="basic", **kwargs)
+
+    def in_depth_search(self, query, **kwargs):
+        """
+        In-depth search method.
+        """
+        return self._search(query, search_depth="in-depth", **kwargs)
+
---
+++ tests/in-depth_search.py
@@ -0,0 +1,17 @@
+import tavily
+import json
+
+# in-depth search
+
+# Initialize client
+tavily = tavily.Client(api_key="123456")
+
+# Define query
+q = "how does the law protect judicial impartiality against individual judges' biases"
+
+# Run API Call
+response = tavily.in_depth_search(q, num_results=3, include_raw_content=False)
+
+# Print Results
+print(json.dumps(response, indent=4))
+
