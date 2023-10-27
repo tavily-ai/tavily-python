1ec5fa6058cd390d34cf1b62e83c553b4623e0ca
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-10-27 03:57:39 +0300

    fix imports relative paths

--- tavily/utils.py
+++ tavily/utils.py
@@ -1,6 +1,6 @@
 import tiktoken
 import json
-from config import DEFAULT_MODEL_ENCODING, DEFAULT_MAX_TOKENS
+from .config import DEFAULT_MODEL_ENCODING, DEFAULT_MAX_TOKENS
 
 
 def get_total_tokens_from_string(string: str, encoding_name: str = DEFAULT_MODEL_ENCODING) -> int:
